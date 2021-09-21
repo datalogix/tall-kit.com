323eb64912719a8192db86643f60d3e4fc38cc5e
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2021-09-21 10:56:56 -0300

    first commit

---
+++ config.production.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'baseUrl' => 'https://tall-kit.github.io',
+    'production' => true,
+
+    // DocSearch credentials
+    'docsearchApiKey' => '',
+    'docsearchIndexName' => '',
+];
---
+++ config.staging.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'baseUrl' => 'https://tall-kit.github.io',
+    'production' => false,
+
+    // DocSearch credentials
+    'docsearchApiKey' => '',
+    'docsearchIndexName' => '',
+];
